9368bd5cb5db6c2991d986a72355eb10789480e7
Stian Lystad <Stianlystad_1@hotmail.com>
2022-10-12 09:13:20 +0000

    add signinform

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moments-stian.herokuapp.com'
+axios.defaults.baseURL = 'https://api-django-stian.herokuapp.com/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
